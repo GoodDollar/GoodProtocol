0c190cddc7c4ce0e26d2669f0c765c2e171176e2
aliberkhsl <aliberkhasil@gmail.com>
2021-05-04 21:58:30 +0300

    add : more test cases

--- test/governancestaking/GovernanceStaking.test.ts
+++ test/governancestaking/GovernanceStaking.test.ts
@@ -21,7 +21,7 @@ describe("GovernanceStaking - staking with GD  and get Rewards in GDAO", () => {
   let dai: Contract;
   let cDAI: Contract;
   let goodReserve: GoodReserveCDai;
-  let governanceStaking:Contract;
+  let governanceStaking: Contract;
   let goodFundManager: Contract;
   let grep: GReputation;
   let avatar,
@@ -88,9 +88,9 @@ describe("GovernanceStaking - staking with GD  and get Rewards in GDAO", () => {
       BLOCK_INTERVAL
     );
     grep = (await ethers.getContractAt(
-        "GReputation",
-        reputation
-      )) as GReputation;
+      "GReputation",
+      reputation
+    )) as GReputation;
     console.log("Deployed goodfund manager", {
       manager: goodFundManager.address
     });
@@ -107,46 +107,84 @@ describe("GovernanceStaking - staking with GD  and get Rewards in GDAO", () => {
     });
 
     console.log("setting permissions...");
-    governanceStaking = await governanceStakingFactory.deploy(grep.address, nameService.address,"DAOStaking","DST");
-   
+    governanceStaking = await governanceStakingFactory.deploy(
+      grep.address,
+      nameService.address,
+      "DAOStaking",
+      "DST"
+    );
 
     setDAOAddress("CDAI", cDAI.address);
     setDAOAddress("DAI", dai.address);
-
-    const ictrl = await ethers.getContractAt(
-      "Controller",
-      controller,
-      schemeMock
-    );
 
     //This set addresses should be another function because when we put this initialization of addresses in initializer then nameservice is not ready yet so no proper addresses
     await goodReserve.setAddresses();
 
     await setDAOAddress("MARKET_MAKER", marketMaker.address);
     await setDAOAddress("FUND_MANAGER", goodFundManager.address);
-    
   });
 
-  it("Should not mint reward when staking contract is not minter ",async()=>{
-        await goodDollar.mint(founder.address,"100")
-        await goodDollar.approve(governanceStaking.address, "100")
-        await governanceStaking.stake("100")
-        await advanceBlocks(5);
-        const error = await governanceStaking.withdrawStake("100").catch(e=>e)
-        expect(error.message).to.have.string(
-            "GReputation: need minter role or be GDAO contract"
-          );
+  it("Should not mint reward when staking contract is not minter ", async () => {
+    await goodDollar.mint(founder.address, "100");
+    await goodDollar.approve(governanceStaking.address, "100");
+    await governanceStaking.stake("100");
+    await advanceBlocks(5);
+    const error = await governanceStaking.withdrawStake("100").catch(e => e);
+    expect(error.message).to.have.string(
+      "GReputation: need minter role or be GDAO contract"
+    );
+  });
 
+  it("Should be able mint rewards after set GDAO staking contract", async () => {
+    await setDAOAddress("GDAO_STAKING", governanceStaking.address);
+    const GDAOBalanceBeforeWithdraw = await grep.balanceOf(founder.address);
+    await governanceStaking.withdrawStake("100");
+    const GDAOBalanceAfterWithdraw = await grep.balanceOf(founder.address);
+
+    expect(GDAOBalanceAfterWithdraw.gt(GDAOBalanceBeforeWithdraw)).to.be.true;
+  });
+
+  it("Avatar should be able to change rewards per block", async () => {
+    const ictrl = await ethers.getContractAt(
+      "Controller",
+      controller,
+      schemeMock
+    );
+    const governanceStakingFactory = await ethers.getContractFactory(
+      "GovernanceStaking"
+    );
+    let encodedCall = governanceStakingFactory.interface.encodeFunctionData(
+      "setRewardsPerBlock",
+      ["10"]
+    );
+    await ictrl.genericCall(governanceStaking.address, encodedCall, avatar, 0);
+    
+    await goodDollar.mint(founder.address, "100");
+    await goodDollar.approve(governanceStaking.address, "100");
+    await governanceStaking.stake("100");
+    await advanceBlocks(4);
+    const GDAOBalanceBeforeWithdraw = await grep.balanceOf(founder.address);
+    await governanceStaking.withdrawStake("100")
+    const GDAOBalanceAfterWithdraw = await grep.balanceOf(founder.address);
+    expect(GDAOBalanceAfterWithdraw.sub(GDAOBalanceBeforeWithdraw).toString()).to.be.equal("50")
+    encodedCall = governanceStakingFactory.interface.encodeFunctionData(
+        "setRewardsPerBlock",
+        ["7"]
+      );
+      await ictrl.genericCall(governanceStaking.address, encodedCall, avatar, 0);
+  });
+
+  it("Should be able to withdraw rewards without withdraw stake", async () => {
+
+    await goodDollar.mint(founder.address, "100");
+    await goodDollar.approve(governanceStaking.address, "100");
+    await governanceStaking.stake("100");
+    await advanceBlocks(4);
+    const GDAOBalanceBeforeWithdraw = await grep.balanceOf(founder.address);
+    await governanceStaking.withdrawRewards()
+    const GDAOBalanceAfterWithdraw = await grep.balanceOf(founder.address);
+    expect(GDAOBalanceAfterWithdraw.sub(GDAOBalanceBeforeWithdraw).toString()).to.be.equal("35")
+    await governanceStaking.withdrawStake("100")
   })
 
-  it("Should be able mint rewards after set GDAO staking contract",async() =>{
-      await setDAOAddress("GDAO_STAKING",governanceStaking.address);
-      const GDAOBalanceBeforeWithdraw = await grep.balanceOf(founder.address)
-      await governanceStaking.withdrawStake("100")
-      const GDAOBalanceAfterWithdraw = await grep.balanceOf(founder.address)
-
-      expect(GDAOBalanceAfterWithdraw.gt(GDAOBalanceBeforeWithdraw)).to.be.true;
-  })
-
-  
 });
