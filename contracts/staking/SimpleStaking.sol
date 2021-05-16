bc3e1546406f699fcf6c0f3dd31d3c8bdc7f9dd7
aliberkhsl <aliberkhasil@gmail.com>
2021-05-16 13:56:53 +0300

    check redeemedtoken if dai or cdai

--- contracts/staking/SimpleStaking.sol
+++ contracts/staking/SimpleStaking.sol
@@ -268,6 +268,8 @@ contract SimpleStaking is AbstractGoodStaking, StakingToken {
 			uint256
 		)
 	{
+		address daiAddress = nameService.getAddress("DAI");
+		address cDAIAddress = nameService.getAddress("CDAI");
 		// otherwise fund manager has to wait for the next interval
 		require(
 			_recipient != address(this),
@@ -283,6 +285,7 @@ contract SimpleStaking is AbstractGoodStaking, StakingToken {
 				ERC20(redeemedToken).transfer(_recipient, redeemedAmount),
 				"collect transfer failed"
 			);
+		require(redeemedToken == daiAddress || redeemedToken == cDAIAddress, "Redeemed token should be DAI or cDAI");
 		emit InterestCollected(
 			_recipient,
 			address(token),
