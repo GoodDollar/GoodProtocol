94d98fa95310a96a8cc9506f02f7c667c9318b02
aliberkhsl <aliberkhasil@gmail.com>
2021-05-18 18:16:30 +0300

    add : more test cases

--- test/staking/SimpleDAIStaking.test.ts
+++ test/staking/SimpleDAIStaking.test.ts
@@ -1184,6 +1184,24 @@ describe("SimpleDAISTAking - staking with cDAI mocks", () => {
     const productivityAfterWithdraw = await goodCompoundStaking.getProductivity(staker.address)
     expect(productivityBeforeStake[0]).to.be.equal(productivityAfterWithdraw[0])
   })
+
+  it("it should be able to stake in Token and withdraw in iToken",async()=>{
+    const stakingAmount = ethers.utils.parseEther("100")
+    await dai["mint(address,uint256)"](staker.address, stakingAmount);
+    await dai.connect(staker).approve(goodCompoundStaking.address, stakingAmount);
+    const productivityBeforeStake = await goodCompoundStaking.getProductivity(staker.address)
+    await goodCompoundStaking
+      .connect(staker)
+      .stake(stakingAmount, 100, false)
+    await goodCompoundStaking
+      .connect(staker)
+      .withdrawStake("623826387", true) 
+    await goodCompoundStaking.connect(staker).withdrawStake("000000000036236363637",false);// 000000000036236363637 is precision loss due to itoken decimals < token decimals
+    const productivityAfterWithdraw = await goodCompoundStaking.getProductivity(staker.address)
+    
+    expect(productivityBeforeStake[0]).to.be.equal(productivityAfterWithdraw[0]) 
+  })
+
   it("should not withdraw interest if the recipient specified by the owner is the staking contract", async () => {
     await advanceBlocks(BLOCK_INTERVAL);
     await setDAOAddress("FUND_MANAGER", founder.address);
