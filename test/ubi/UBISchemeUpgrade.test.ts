9b3335ccaf744a58a5f5f3ca3c40f30a53b9ffc4
aliberkhsl <aliberkhasil@gmail.com>
2021-05-19 18:24:42 +0300

    refactor : formatting

--- test/ubi/UBISchemeUpgrade.test.ts
+++ test/ubi/UBISchemeUpgrade.test.ts
@@ -2,34 +2,28 @@ import { default as hre, ethers, upgrades } from "hardhat";
 import { BigNumber, Contract, Signer } from "ethers";
 import { deployMockContract, MockContract } from "ethereum-waffle";
 import { expect } from "chai";
-import {
-} from "../../types";
+import {} from "../../types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
-import { createDAO, increaseTime, deployUBI,deployOldUBI } from "../helpers";
+import { createDAO, increaseTime, deployUBI, deployOldUBI } from "../helpers";
 const BN = ethers.BigNumber;
 export const NULL_ADDRESS = ethers.constants.AddressZero;
-
-
-
-
-
 const MAX_INACTIVE_DAYS = 3;
 const ONE_DAY = 86400;
 
-
 describe("UBIScheme Upgrade Process", () => {
-  let ubi, controller, ubiUpgrade,addWhitelisted,deployedDAO,nameService,setSchemes;
+  let ubi,
+    controller,
+    ubiUpgrade,
+    addWhitelisted,
+    deployedDAO,
+    nameService,
+    setSchemes;
   let goodDollar, firstClaimPool;
   let reputation;
-  let founder,
-    claimer1,
-    claimer2,
-    signers,
-    ubiScheme;
+  let founder, claimer1, claimer2, signers, ubiScheme;
   before(async () => {
-
-    [founder, claimer1,claimer2, ...signers] = await ethers.getSigners();
-    const block = await ethers.provider.getBlock('latest');
+    [founder, claimer1, claimer2, ...signers] = await ethers.getSigners();
+    const block = await ethers.provider.getBlock("latest");
     const roundToNextMidnight = block.timestamp % (60 * 60 * 24);
     await increaseTime(60 * 60 * 24 - roundToNextMidnight);
     deployedDAO = await createDAO();
@@ -38,23 +32,21 @@ describe("UBIScheme Upgrade Process", () => {
       genericCall: gn,
       reputation: rep,
       setDAOAddress,
-      setSchemes:sc,
-      addWhitelisted:wl,
-      gd,
+      setSchemes: sc,
+      addWhitelisted: wl,
+      gd
     } = deployedDAO;
     addWhitelisted = wl;
     nameService = ns;
     setSchemes = sc;
     goodDollar = await ethers.getContractAt("IGoodDollar", gd);
-
-   
   });
 
   it("should deploy the ubi", async () => {
     const oldUbi = await deployOldUBI(deployedDAO);
     ubi = oldUbi.ubiScheme;
     firstClaimPool = oldUbi.firstClaim;
-  
+
     let isActive = await ubi.isActive();
     expect(isActive).to.be.false;
     await setSchemes([ubi.address]);
@@ -68,10 +60,8 @@ describe("UBIScheme Upgrade Process", () => {
     expect(startDate.toISOString()).to.have.string("T12:00:00.000Z"); //contract set itself to start at noon GMT
     expect(newUbi.toString()).to.be.equal(ubi.address);
     expect(isActive).to.be.true;
-    const gdBalance = await goodDollar.balanceOf(ubi.address)
-    expect(gdBalance.toString()).to.be.equal(
-      '1000000'
-    );
+    const gdBalance = await goodDollar.balanceOf(ubi.address);
+    expect(gdBalance.toString()).to.be.equal("1000000");
   });
 
   it("should deploy upgrade", async () => {
@@ -93,7 +83,7 @@ describe("UBIScheme Upgrade Process", () => {
   it("should start once registered scheme and prev scheme expired", async () => {
     const block = await ethers.provider.getBlock("latest");
     const now = block.timestamp;
-    let {setSchemes} = deployedDAO
+    let { setSchemes } = deployedDAO;
     await setSchemes([ubiUpgrade.address]);
     const res = await ubiUpgrade.upgrade(ubi.address);
     const newUbi = await firstClaimPool.ubi();
@@ -107,11 +97,13 @@ describe("UBIScheme Upgrade Process", () => {
     const oldUbiBalance = await goodDollar.balanceOf(ubi.address);
     const newUbiBalance = await goodDollar.balanceOf(ubiUpgrade.address);
     expect(oldUbiBalance.toNumber()).to.equal(0);
-    expect(newUbiBalance.toNumber()).to.equal(1000000); 
+    expect(newUbiBalance.toNumber()).to.equal(1000000);
   });
 
   it("should have set new firstclaim await correctly", async () => {
-    expect(await firstClaimPool.claimAmount().then(_ => _.toNumber())).to.equal(1000);
+    expect(await firstClaimPool.claimAmount().then(_ => _.toNumber())).to.equal(
+      1000
+    );
   });
 
   it("should not be able to call upgrade again", async () => {
@@ -120,7 +112,10 @@ describe("UBIScheme Upgrade Process", () => {
   });
 
   it("should not be able to claim until 12pm", async () => {
-    const res = await ubiUpgrade.connect(claimer1).claim().catch(e => e.message);
+    const res = await ubiUpgrade
+      .connect(claimer1)
+      .claim()
+      .catch(e => e.message);
     expect(res).to.contain("not in periodStarted");
   });
 
@@ -138,7 +133,4 @@ describe("UBIScheme Upgrade Process", () => {
     const res = await ubiUpgrade.connect(claimer1).claim();
     expect(res).to.not.be.false;
   });
-
-
-  
 });
