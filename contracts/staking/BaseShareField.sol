2270b271295da3ddb160930d6ef8854ca2534438
aliberkhsl <aliberkhasil@gmail.com>
2021-05-26 16:28:00 +0300

    refactor: refactor calculations of baseSharefield and add edge test cases

--- contracts/staking/BaseShareField.sol
+++ contracts/staking/BaseShareField.sol
@@ -5,7 +5,6 @@ import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/utils/math/Math.sol";
 import "../utils/DAOContract.sol";
 import "../utils/DSMath.sol";
-
 interface FundManager {
 	function rewardsForStakingContract(address _staking)
 		external
@@ -66,19 +65,24 @@ contract BaseShareField is DAOContract, DSMath {
 		FundManager fm = FundManager(nameService.getAddress("FUND_MANAGER"));
 		(uint256 rewardsPerBlock, uint256 blockStart, uint256 blockEnd, ) =
 			fm.rewardsForStakingContract(address(this));
-		if (block.number >= blockStart && lastRewardBlock < blockStart) {
-			lastRewardBlock = blockStart;
-		}
+		
+		uint256 _lastRewardBlock = lastRewardBlock < blockStart &&
+			block.number >= blockStart
+			? blockStart
+			: lastRewardBlock;
+		uint256 curRewardBlock =
+			block.number > blockEnd ? blockEnd : block.number;
+		if (curRewardBlock < blockStart) return;
+		if (_lastRewardBlock >= blockEnd) return;
+		
+		uint256 multiplier = curRewardBlock - _lastRewardBlock; // Blocks passed since last reward block
+		uint256 reward = multiplier * (rewardsPerBlock * 1e16); // rewardsPerBlock is in G$ which is only 2 decimals, we turn it into 18 decimals by multiplying 1e16
 
-		if (block.number >= blockStart && blockEnd >= block.number) {
-			uint256 multiplier = block.number - lastRewardBlock; // Blocks passed since last reward block
-			uint256 reward = multiplier * (rewardsPerBlock * 1e16); // rewardsPerBlock is in G$ which is only 2 decimals, we turn it into 18 decimals by multiplying 1e16
-
-			accAmountPerShare =
-				accAmountPerShare +
-				rdiv(reward, totalProductivity * (10**tokenDecimalDifference)); // Increase totalproductivity decimals if it is less than 18 decimals then accAmountPerShare in 27 decimals
-		}
-		lastRewardBlock = block.number;
+		accAmountPerShare =
+			accAmountPerShare +
+			rdiv(reward, totalProductivity * (10**tokenDecimalDifference)); // Increase totalproductivity decimals if it is less than 18 decimals then accAmountPerShare in 27 decimals
+		
+		lastRewardBlock = curRewardBlock;
 	}
 
 	/**
