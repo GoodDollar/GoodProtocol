b69e4690b84f08bde1cea80fe0f4589738299479
aliberkhsl <aliberkhasil@gmail.com>
2021-06-08 18:27:15 +0300

    add : aave staking contract

--- contracts/Interfaces.sol
+++ contracts/Interfaces.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-
+import {DataTypes} from './utils/DataTypes.sol';
 pragma solidity >=0.8.0;
 
 pragma experimental ABIEncoderV2;
@@ -291,4 +291,62 @@ interface AggregatorV3Interface {
 		);
 
 	function latestAnswer() external view returns (int256);
+}
+/**
+	@dev interface for AAVE lending Pool
+ */
+interface ILendingPool {
+
+	/**
+   * @dev Deposits an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
+   * - E.g. User deposits 100 USDC and gets in return 100 aUSDC
+   * @param asset The address of the underlying asset to deposit
+   * @param amount The amount to be deposited
+   * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
+   *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
+   *   is a different wallet
+   * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
+   *   0 if the action is executed directly by the user, without any middle-man
+   **/
+  function deposit(
+    address asset,
+    uint256 amount,
+    address onBehalfOf,
+    uint16 referralCode
+  ) external;
+
+  /**
+   * @dev Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
+   * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
+   * @param asset The address of the underlying asset to withdraw
+   * @param amount The underlying amount to be withdrawn
+   *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
+   * @param to Address that will receive the underlying, same as msg.sender if the user
+   *   wants to receive it on his own wallet, or a different address if the beneficiary is a
+   *   different wallet
+   * @return The final amount withdrawn
+   **/
+  function withdraw(
+    address asset,
+    uint256 amount,
+    address to
+  ) external returns (uint256);
+ 
+  /**
+   * @dev Returns the state and configuration of the reserve
+   * @param asset The address of the underlying asset of the reserve
+   * @return The state of the reserve
+   **/
+  function getReserveData(address asset) external view returns (DataTypes.ReserveData memory);
+}
+
+interface IAToken is ERC20{
+
+ /**
+   * @dev Returns the scaled balance of the user. The scaled balance is the sum of all the
+   * updated stored balance divided by the reserve's liquidity index at the moment of the update
+   * @param user The user whose balance is calculated
+   * @return The scaled balance of the user
+   **/
+  function scaledBalanceOf(address user) external view returns (uint256);
 }
---
+++ contracts/staking/aave/GoodAaveStaking.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+import "../SimpleStaking.sol";
+import "../../Interfaces.sol";
+import "../../utils/DataTypes.sol";
+/**
+ * @title Staking contract that donates earned interest to the DAO
+ * allowing stakers to deposit Token
+ * or withdraw their stake in Token
+ * the contracts buy cToken and can transfer the daily interest to the  DAO
+ */
+contract GoodAaveStaking is SimpleStaking {
+	// Address of the TOKEN/USD oracle from chainlink
+	address public tokenUsdOracle;
+
+	//LendingPool of aave
+    ILendingPool public lendingPool;
+
+    bool public initialized = false;
+	/**
+	 * @param _token Token to swap DEFI token
+	 * @param _lendingPool LendingPool address
+	 * @param _ns Address of the NameService
+	 * @param _tokenName Name of the staking token which will be provided to staker for their staking share
+	 * @param _tokenSymbol Symbol of the staking token which will be provided to staker for their staking share
+	 * @param _tokenSymbol Determines blocks to pass for 1x Multiplier
+	 * @param _tokenUsdOracle address of the TOKEN/USD oracle
+	 * @param _collectInterestGasCost Gas cost for the collect interest of this staking contract
+	 */
+	function init(
+		address _token,
+		address _lendingPool,
+		NameService _ns,
+		string memory _tokenName,
+		string memory _tokenSymbol,
+		uint64 _maxRewardThreshold,
+		address _tokenUsdOracle,
+		uint32 _collectInterestGasCost
+	) public {
+		require(initialized == false , 'Already initialized');
+        lendingPool = ILendingPool(lendingPool);
+        DataTypes.ReserveData memory reserve = lendingPool.getReserveData(_token);
+        initialize(
+			_token,
+			reserve.aTokenAddress,
+			_ns,
+			_tokenName,
+			_tokenSymbol,
+			_maxRewardThreshold,
+			_collectInterestGasCost
+		);
+		//above  initialize going  to revert on second call, so this is safe
+		tokenUsdOracle = _tokenUsdOracle;
+		initialized = true;
+	}
+
+    /**
+	 * @dev stake some Token
+	 * @param _amount of Token to stake
+	 */
+	function mintInterestToken(uint256 _amount) internal override {
+		lendingPool.deposit(address(token), _amount, address(this), 0);
+
+	}
+
+    /**
+	 * @dev redeem Token from aave
+	 * @param _amount of token to redeem in Token
+	 */
+	function redeem(uint256 _amount) internal override {
+		lendingPool.withdraw(address(token), _amount, address(this));
+	}
+
+    /**
+	 * @dev Function to redeem aToken for DAI, so reserve knows how to handle it. (reserve can handle dai or cdai)
+	 * @dev _amount of token in iToken
+	 * @return return address of the DAI and amount of the DAI
+	 */
+	function redeemUnderlyingToDAI(uint256 _amount)
+		internal
+		override
+		returns (address, uint256)
+	{
+		
+		lendingPool.withdraw(address(token), _amount, address(this));
+		uint256 redeemedAmount = token.balanceOf(address(this));
+		address daiAddress = nameService.getAddress("DAI");
+		address[] memory path = new address[](2);
+		path[0] = address(token);
+		path[1] = daiAddress;
+		Uniswap uniswapContract =
+			Uniswap(nameService.getAddress("UNISWAP_ROUTER"));
+		token.approve(address(uniswapContract), redeemedAmount);
+		uint256[] memory swap =
+			uniswapContract.swapExactTokensForTokens(
+				redeemedAmount,
+				0,
+				path,
+				address(this),
+				block.timestamp
+			);
+
+		uint256 dai = swap[1];
+		require(dai > 0, "token selling failed");
+		return (daiAddress, swap[1]);
+	}
+
+    /**
+	 * @dev returns decimals of token.
+	 */
+	function tokenDecimal() internal view override returns (uint256) {
+		ERC20 token = ERC20(address(token));
+		return uint256(token.decimals());
+	}
+
+	/**
+	 * @dev returns decimals of interest token.
+	 */
+	function iTokenDecimal() internal view override returns (uint256) {
+		ERC20 aToken = ERC20(address(iToken));
+		return uint256(aToken.decimals());
+	}
+    function currentGains(
+		bool _returnTokenBalanceInUSD,
+		bool _returnTokenGainsInUSD
+	)
+		public
+		view
+		override
+		returns (
+			uint256,
+			uint256,
+			uint256,
+			uint256,
+			uint256
+		)
+	{
+		IAToken aToken = IAToken(address(iToken));
+        DataTypes.ReserveData memory reserve = lendingPool.getReserveData(address(token));
+		uint256 scaledBalance = aToken.scaledBalanceOf(address(this));
+        
+		uint256 tokenBalance =scaledBalance * reserve.liquidityIndex;
+		uint256 balanceInUSD =
+			_returnTokenBalanceInUSD
+				? getTokenValueInUSD(tokenUsdOracle, tokenBalance)
+				: 0;
+		if (tokenBalance <= totalProductivity) {
+			return (0, 0, tokenBalance, balanceInUSD, 0);
+		}
+		uint256 tokenGains = tokenBalance - totalProductivity;
+		
+		uint256 tokenGainsInUSD =
+			_returnTokenGainsInUSD
+				? getTokenValueInUSD(tokenUsdOracle, tokenGains)
+				: 0;
+		return (
+			tokenGains, // since token gains == atoken gains
+			tokenGains,
+			tokenBalance,
+			balanceInUSD,
+			tokenGainsInUSD
+		);
+	}
+
+    function getGasCostForInterestTransfer()
+		external
+		view
+		override
+		returns (uint32)
+	{
+		return collectInterestGasCost;
+	}
+
+    /**
+	 * @dev Calculates worth of given amount of iToken in Token
+	 * @param _amount Amount of token to calculate worth in Token
+	 * @return Worth of given amount of token in Token
+	 */
+	function iTokenWorthInToken(uint256 _amount)
+		public
+		view
+		override
+		returns (uint256)
+	{
+		
+		return _amount; // since aToken is peg to Token 1:1 return exact amount
+	}
+	
+}
