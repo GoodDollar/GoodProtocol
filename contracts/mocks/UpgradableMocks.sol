b0d33a686245cfb876c7d32b9b32b3b7ea3edef6
sirpy <hadarr@gmail.com>
2021-10-04 10:43:27 +0300

    add: proxyfactory for det deploymented address

---
+++ contracts/mocks/UpgradableMocks.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
+
+import "../utils/DAOUpgradeableContract.sol";
+
+contract UpgradableMock is DAOUpgradeableContract {
+	address public owner;
+
+	function decimals() public pure returns (uint256) {
+		return 8;
+	}
+
+	function initialize(address _owner) public initializer {
+		owner = _owner;
+	}
+
+	function _authorizeUpgrade(address) internal virtual override {}
+}
+
+contract UpgradableMock2 is DAOUpgradeableContract {
+	function decimals() public pure returns (uint256) {
+		return 18;
+	}
+
+	function _authorizeUpgrade(address) internal virtual override {
+		require(
+			msg.sender == address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266),
+			"not authorized to upgrade"
+		);
+	}
+}
---
+++ test/utils/ProxyFactory.test.ts
@@ -0,0 +1,68 @@
+import hre, { ethers } from "hardhat";
+import { BigNumber, Contract } from "ethers";
+import { expect } from "chai";
+import { deployMockContract, MockContract } from "ethereum-waffle";
+
+import { ProxyFactory1967, ERC1967Proxy } from "../../types";
+const BN = ethers.BigNumber;
+const MaxUint256 = ethers.constants.MaxUint256;
+export const NULL_ADDRESS = ethers.constants.AddressZero;
+export const BLOCK_INTERVAL = 30;
+
+describe("proxyfactory", () => {
+  let factory: ProxyFactory1967, signers;
+  let proxy;
+  before(async () => {
+    signers = await ethers.getSigners();
+    const f = await ethers.getContractFactory("ProxyFactory1967");
+    factory = (await f.deploy()) as unknown as ProxyFactory1967;
+    console.log(signers[0].address);
+  });
+
+  it("should deploy proxy with impl", async () => {
+    const c1 = await (
+      await ethers.getContractFactory("UpgradableMock")
+    ).deploy();
+    const deployTX = await (
+      await factory.deployProxy(1, c1.address, ethers.utils.toUtf8Bytes(""))
+    ).wait();
+    const proxyAddr = deployTX.events.find(_ => _.event === "ProxyCreated").args
+      .proxy;
+    proxy = await ethers.getContractAt("UpgradableMock", proxyAddr);
+    expect(await proxy.decimals()).to.eq(8);
+  });
+
+  it("should be able to upgrade impl", async () => {
+    const c2 = await (
+      await ethers.getContractFactory("UpgradableMock2")
+    ).deploy();
+    await proxy.upgradeTo(c2.address);
+    expect(await proxy.decimals()).to.eq(18);
+  });
+
+  it("should not be upgradable if not authorized", async () => {
+    const c1 = await (
+      await ethers.getContractFactory("UpgradableMock")
+    ).deploy();
+    await expect(proxy.connect(signers[1]).upgradeTo(c1.address)).revertedWith(
+      "authorized"
+    );
+    expect(await proxy.decimals()).to.eq(18);
+  });
+
+  it("should deploy proxy with impl and initialize it", async () => {
+    const c1 = await (
+      await ethers.getContractFactory("UpgradableMock")
+    ).deploy();
+    const encoded = c1.interface.encodeFunctionData("initialize", [
+      signers[2].address
+    ]);
+    const deployTX = await (
+      await factory.deployProxy(3, c1.address, encoded)
+    ).wait();
+    const proxyAddr = deployTX.events.find(_ => _.event === "ProxyCreated").args
+      .proxy;
+    proxy = await ethers.getContractAt("UpgradableMock", proxyAddr);
+    expect(await proxy.owner()).to.eq(signers[2].address);
+  });
+});
