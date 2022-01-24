f1c2466ad4db44e17d19313475e4c8205013d3f9
sirpy <hadarr@gmail.com>
2022-01-24 14:50:56 +0200

    add: verify helpers

--- truffle-config.js
+++ truffle-config.js
@@ -70,6 +70,18 @@ module.exports = {
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
     },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5000000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
     fuse: {
       provider: () => new HDWalletProvider(mnemonic, `https://rpc.fuse.io`),
       network_id: 122, // Ropsten's id
