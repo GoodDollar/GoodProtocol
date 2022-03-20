117598b906faa7b45d0514d2277117edef31c90b
sirpy <hadarr@gmail.com>
2022-03-20 23:00:18 +0200

    RELEASE: minimized abi json

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -4,11 +4,12 @@
 #  - Shchema: https://github.com/trufflesuite/truffle/tree/develop/packages/truffle-contract-schema
 #  - bytecode vs deployedBytecode: https://ethereum.stackexchange.com/questions/32234/difference-between-bytecode-and-runtime-bytecode
 # FILES=`find $1 -name '*.json' ! -name '*.dbg.json'`
+mkdir artifacts/abis && true
 find $1 -name "*.json" ! -name '*.*.json' |while read fname; do
   echo "$fname"
   #[ -z "${i%%*.min.json}" ] && continue # already minified
   f=${fname##*/}
-  m=./build/contracts/${f%%.json}.min.json
+  m=./artifacts/abis/${f%%.json}.min.json
   echo "Minimizing contract json artifact: $fname"
   echo "Original size: $(wc -c "$fname")"
   ./node_modules/node-jq/bin/jq -c 'del(.bytecode,.deployedBytecode,.ast,.legacyAST,.source,.deployedSourceMap,.userdoc,.sourcePath)' $fname > $m
