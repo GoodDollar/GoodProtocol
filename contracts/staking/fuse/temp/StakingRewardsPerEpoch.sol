582142c9ba50f980c24b2d70e2fb7213eb3f09ab
Oleg Bedrin <simplavero@gmail.com>
2022-03-23 12:49:41 +0300

    rename just to make easier review

--- contracts/staking/fuse/temp/StakingRewardsPerEpoch.sol
+++ contracts/staking/fuse/temp/StakingRewardsPerEpoch.sol
@@ -5,14 +5,13 @@ import "./StakingRewards.sol";
 
 contract StakingRewardsPerEpoch is StakingRewards {
 
-
   struct StakeInfoPerEpoch {
     uint256 pendingStake;
     uint256 giveBackRatio;
     uint256 indexOfLastEpochStaked;
   }
 
-  uint256 public lastCollectUBIInterestIndex;
+  uint256 public lastEpochIndex;
   uint256 public pendingStakes;
 
   mapping (address => StakeInfoPerEpoch) public stakersInfoPerEpoch;
@@ -34,6 +33,27 @@ contract StakingRewardsPerEpoch is StakingRewards {
     stakersInfoPerEpoch[_from].pendingStake -= _amount;
     stakingToken.safeTransfer(_from, _amount);
     emit PendingWithdrawn(_from, _amount);
+  }
+
+  function notifyRewardAmount(uint256 reward) external virtual onlyRole(GUARDIAN_ROLE) updateReward(address(0)) {
+      if (block.timestamp >= periodFinish) {
+          rewardRate = reward / rewardsDuration;
+      } else {
+          uint256 remaining = periodFinish - block.timestamp;
+          uint256 leftover = remaining * rewardRate;
+          rewardRate = (reward + leftover) / rewardsDuration;
+      }
+
+      // Ensure the provided reward amount is not more than the balance in the contract.
+      // This keeps the reward rate in the right range, preventing overflows due to
+      // very high values of rewardRate in the earned and rewardsPerToken functions;
+      // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+      uint balance = rewardsToken.balanceOf(address(this));
+      require(rewardRate <= balance / rewardsDuration, "Provided reward too high");
+
+      lastUpdateTime = block.timestamp;
+      periodFinish = block.timestamp + rewardsDuration;
+      emit RewardAdded(reward);
   }
 
   event PendingStaked(address indexed user, uint256 amount);
