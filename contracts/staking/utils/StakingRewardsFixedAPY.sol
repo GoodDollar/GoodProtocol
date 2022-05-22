3861fed49bb33b597f763f25cf7ac11492dd4055
sirpy <hadarr@gmail.com>
2022-05-22 12:31:14 +0300

    fix: not calling updateReward and not using blocks

--- contracts/staking/utils/StakingRewardsFixedAPY.sol
+++ contracts/staking/utils/StakingRewardsFixedAPY.sol
@@ -2,36 +2,48 @@
 pragma solidity >=0.8.0;
 
 contract StakingRewardsFixedAPY {
-
 	// the users stake information
 	struct StakerInfo {
-		uint256 reward; // the reward amount which should be transfered to the user
-		uint256 balance; // the amount of user active stake
-		uint256 rewardPerTokenPaid; // rewards that accounted already so should be substracted
+		uint128 reward; // the reward amount which should be transfered to the user
+		uint128 balance; // the amount of user active stake
+		uint128 rewardPerTokenPaid; // rewards that accounted already so should be substracted
 		// while calculating rewards of staker
+		uint128 rewardsMinted;
 	}
-
-	// precision constant for math
-	uint256 public constant PRECISION = 1e18;
 
 	// the user info sheet
 	mapping(address => StakerInfo) public stakersInfo;
 
+	// precision constant for math
+	uint128 public constant PRECISION = 1e18;
+
 	// last timestamp this staking contract was updated and rewards were calculated
-	uint256 public lastUpdateTime;
+	uint128 public lastUpdateBlock;
 
 	// the amount of reward per token
-	uint256 public rewardPerTokenStored;
+	uint128 public rewardPerTokenStored;
 
 	// total supply of active stakes
-	uint256 public totalStaked;
+	uint128 public totalStaked;
 
 	// annual percentage yield - rate of return on stake over one year
 	// in BPS format. e.g. apy = 500 means 5% (500/10000)
-	uint256 internal _apy;
+	uint128 public apy;
 
 	// interest rate per one block. Equal to _apy * 1e14 / numberOfBlocksPerYear
-	uint256 public interestRatePerBlock;
+	uint128 public interestRatePerBlock;
+
+	uint128 public numberOfBlocksPerYear;
+
+	constructor(uint128 _apy, uint128 _numberOfBlocksPerYear) {
+		_setAPY(_apy, _numberOfBlocksPerYear);
+	}
+
+	function _setAPY(uint128 _apy, uint128 _numberOfBlocksPerYear) internal {
+		apy = _apy;
+		numberOfBlocksPerYear = _numberOfBlocksPerYear;
+		interestRatePerBlock = (_apy * 1e14) / _numberOfBlocksPerYear;
+	}
 
 	modifier updateReward(address account) {
 		_updateReward(account);
@@ -43,10 +55,13 @@ contract StakingRewardsFixedAPY {
 			return rewardPerTokenStored;
 		}
 
+		//earned in timespan = blocksPassed * interestRatePerBlock * totalStaked/PRECISION
+		//earned perToken = earnedInTimeSpan*PRECISION/totalStaked
+		//PRECISION and totalStaked cancel out
 		return
-			rewardPerTokenStored +=
-				(block.timestamp - lastUpdateTime) *
-				interestRatePerBlock;
+			rewardPerTokenStored += uint128(
+				(block.number - lastUpdateBlock) * interestRatePerBlock
+			);
 	}
 
 	/**
@@ -57,41 +72,56 @@ contract StakingRewardsFixedAPY {
 	function earned(address account) public returns (uint256) {
 		return
 			(stakersInfo[account].balance *
-      (_rewardPerToken() - stakersInfo[account].rewardPerTokenPaid)) /
+				(_rewardPerToken() - stakersInfo[account].rewardPerTokenPaid)) /
 			PRECISION;
 	}
 
 	// this function updates the reward for the specific user
 	function _updateReward(address _account) internal virtual {
-		rewardPerTokenStored = _rewardPerToken();
-		lastUpdateTime = block.timestamp;
+		rewardPerTokenStored = uint128(_rewardPerToken());
+		lastUpdateBlock = uint128(block.number);
 		if (_account != address(0)) {
-			stakersInfo[_account].reward += earned(_account);
+			stakersInfo[_account].reward += uint128(earned(_account));
 			stakersInfo[_account].rewardPerTokenPaid = rewardPerTokenStored;
 		}
 	}
 
-	function _withdraw(address _from, uint256 _amount) internal virtual {
+	function _withdraw(address _from, uint256 _amount)
+		internal
+		virtual
+		updateReward(_from)
+	{
 		require(_amount > 0, "Cannot withdraw 0");
-		totalStaked -= _amount;
-		stakersInfo[_from].balance -= _amount;
+		totalStaked -= uint128(_amount);
+		stakersInfo[_from].balance -= uint128(_amount);
 	}
 
-	function _stake(address _from, uint256 _amount) internal virtual {
+	function _stake(address _from, uint256 _amount)
+		internal
+		virtual
+		updateReward(_from)
+	{
 		require(_amount > 0, "Cannot stake 0");
-		totalStaked += _amount;
-		stakersInfo[_from].balance += _amount;
+		totalStaked += uint128(_amount);
+		stakersInfo[_from].balance += uint128(_amount);
 	}
 
-	function _getReward(address _to) internal virtual returns (uint256 reward) {
+	function _getReward(address _to)
+		internal
+		virtual
+		updateReward(_to)
+		returns (uint256 reward)
+	{
 		// return and reset the reward if there is any
 		reward = stakersInfo[_to].reward;
-		if (reward > 0) {
-			stakersInfo[_to].reward = 0;
-		}
+		stakersInfo[_to].reward = 0;
+		stakersInfo[_to].rewardsMinted += uint128(reward);
 	}
 
-	// function lastTimeRewardApplicable() public view returns (uint256) {
-	//   return block.timestamp < periodFinish ? block.timestamp : periodFinish;
-	// }
+	/**
+	 * @dev keep track of debt to user
+	 */
+	function _setReward(address _to, uint256 _amount) internal virtual {
+		stakersInfo[_to].reward += uint128(_amount);
+	}
 }
