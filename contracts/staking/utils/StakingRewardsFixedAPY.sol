66a397c1d80a9f981fef5c8738c49bc601506c72
sirpy <hadarr@gmail.com>
2022-05-25 16:55:22 +0300

    Squashed commit of the following:
    commit 15d408238dbac5995a5a15a758e471d346530336
    Author: sirpy <hadarr@gmail.com>
    Date:   Sun May 22 21:40:26 2022 +0300
    
        refactor: rename to undoreward
    
    commit f7e48c1e75dabecd05d1da1aa334e916a3688dfe
    Author: sirpy <hadarr@gmail.com>
    Date:   Sun May 22 14:59:19 2022 +0300
    
        fix: rewards minted and ensure success withdraw
    
    commit ef4c74e6017eee4e6c348c3791451b19b1b1c792
    Author: sirpy <hadarr@gmail.com>
    Date:   Sun May 22 14:43:45 2022 +0300
    
        add: switch to compounding interest
    
    commit 84fe76a8e577d2cd24baa1c278c12b5ca6a94a22
    Author: sirpy <hadarr@gmail.com>
    Date:   Sun May 22 12:35:30 2022 +0300
    
        add: integrate with stakingrewards
    
    commit 3861fed49bb33b597f763f25cf7ac11492dd4055
    Author: sirpy <hadarr@gmail.com>
    Date:   Sun May 22 12:31:14 2022 +0300
    
        fix: not calling updateReward and not using blocks
    
    commit 924c30606fbd7734169eff32604afd5dc561df4f
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Fri May 20 12:06:30 2022 -0400
    
        Insert inheritance
    
    commit 90572a4f0d54fb30875f0e8ac9eef5fa26bd2acf
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Fri May 20 10:11:49 2022 -0400
    
        Staking rewards changes according to comments
    
    commit afc5b7a90a6eeb2a1e6770a3085c69af4edcce35
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Fri May 20 09:12:09 2022 -0400
    
        Change contract name
    
    commit 580b02713b055559a8376897fb5bf783b522aa9e
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Thu May 19 20:42:46 2022 -0400
    
        Small comment fix
    
    commit 1dee3f5596ef9d10ee8ef7363f69b18362c1317b
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Thu May 19 19:15:50 2022 -0400
    
        Remove stake and withdraw events that exist on gov staking
    
    commit c95678c670d2568dd661c8ea4d8459316910468a
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Wed May 18 22:07:50 2022 -0400
    
        Fix indentation
    
    commit 1c680cccf8c2b27f8cad33be599628d0a2bf3c7e
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Wed May 18 22:03:08 2022 -0400
    
        Adjust StakingRewardsPerEpoch from epochs to blocks
    
    commit e3d170d5df08b25f8f20cc63237898cd4dcdd886
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Wed May 18 19:47:07 2022 -0400
    
        Comment fix
    
    commit e7ca5a7b5f295d1492eca6c90254d945bd5ed452
    Author: shloyem <39220943+Shloyem@users.noreply.github.com>
    Date:   Wed May 18 19:40:35 2022 -0400
    
        Duplicate gov staking and add rewardPerEpoch with small fixes

---
+++ contracts/staking/utils/StakingRewardsFixedAPY.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "./Math64X64.sol";
+
+contract StakingRewardsFixedAPY {
+	using Math64x64 for int128;
+
+	// the users stake information
+	struct StakerInfo {
+		uint128 reward; // the reward amount which should be transfered to the user
+		uint128 balance; // the amount of user active stake
+		uint128 rewardPerTokenPaid; // rewards that accounted already so should be substracted
+		// while calculating rewards of staker
+		uint128 rewardsMinted;
+	}
+
+	// the user info sheet
+	mapping(address => StakerInfo) public stakersInfo;
+
+	// precision constant for math
+	uint128 public constant PRECISION = 1e18;
+
+	// last block this staking contract was updated and rewards were calculated
+	uint128 public lastUpdateBlock;
+
+	// the amount of reward per token
+	uint128 public rewardPerTokenStored;
+
+	// total supply of active stakes
+	uint128 public totalStaked;
+	uint256 public principle; //total earning compounding interest;
+
+	// annual percentage yield - rate of return on stake over one year;
+
+	// interest rate per one block in 1e18 precision.
+	//for example APY=5% then per block = nroot(1+0.05,numberOfBlocksPerYear)
+	//nroot(1.05,6000000) = 1.000000008131694
+	//in 1e18 = 1000000008131694000
+	int128 public interestRatePerBlockX64;
+
+	constructor(uint128 _interestRatePerBlock) {
+		_setAPY(_interestRatePerBlock);
+	}
+
+	function _setAPY(uint128 _interestRatePerBlock)
+		internal
+		updateReward(address(0))
+	{
+		interestRatePerBlockX64 = Math64x64.divu(_interestRatePerBlock, 1e18); //convert to signed int x64
+	}
+
+	modifier updateReward(address account) {
+		_updateReward(account);
+		_;
+	}
+
+	function _rewardPerToken() internal returns (uint256) {
+		if (totalStaked == 0) {
+			return rewardPerTokenStored;
+		}
+
+		//earned in timespan = interestRatePerBlock^blocksPassed * principle/PRECISION
+		//earned perToken = earnedInTimeSpan*PRECISION/totalStaked
+		//PRECISION cancels out
+		int128 compound = interestRatePerBlockX64.pow(
+			block.number - lastUpdateBlock
+		);
+		uint256 earnedInterest = compound.mulu(principle);
+		principle += earnedInterest;
+
+		return rewardPerTokenStored += uint128(earnedInterest / totalStaked); //principle is already in PRECISION
+	}
+
+	/**
+	 * @dev The function allows anyone to calculate the exact amount of reward
+	 * earned.
+	 * @param account A staker address
+	 */
+	function earned(address account) public returns (uint256) {
+		return
+			(stakersInfo[account].balance *
+				(_rewardPerToken() - stakersInfo[account].rewardPerTokenPaid)) /
+			PRECISION;
+	}
+
+	// this function updates the reward for the specific user
+	function _updateReward(address _account) internal virtual {
+		rewardPerTokenStored = uint128(_rewardPerToken());
+		lastUpdateBlock = uint128(block.number);
+		if (_account != address(0)) {
+			stakersInfo[_account].reward += uint128(earned(_account));
+			stakersInfo[_account].rewardPerTokenPaid = rewardPerTokenStored;
+		}
+	}
+
+	function _withdraw(address _from, uint256 _amount)
+		internal
+		virtual
+		updateReward(_from)
+	{
+		require(_amount > 0, "Cannot withdraw 0");
+		totalStaked -= uint128(_amount);
+		principle -= _amount * PRECISION;
+		stakersInfo[_from].balance -= uint128(_amount);
+	}
+
+	function _stake(address _from, uint256 _amount)
+		internal
+		virtual
+		updateReward(_from)
+	{
+		require(_amount > 0, "Cannot stake 0");
+		totalStaked += uint128(_amount);
+		principle += _amount * PRECISION;
+
+		stakersInfo[_from].balance += uint128(_amount);
+	}
+
+	function _getReward(address _to)
+		internal
+		virtual
+		updateReward(_to)
+		returns (uint256 reward)
+	{
+		// return and reset the reward if there is any
+		reward = stakersInfo[_to].reward;
+		stakersInfo[_to].reward = 0;
+		stakersInfo[_to].rewardsMinted += uint128(reward);
+		principle -= reward * PRECISION; //rewards are part of the compounding interest
+	}
+
+	/**
+	 * @dev keep track of debt to user in case reward minting failed
+	 */
+	function _undoReward(address _to, uint256 _amount) internal virtual {
+		stakersInfo[_to].reward += uint128(_amount);
+		stakersInfo[_to].rewardsMinted -= uint128(_amount);
+		principle += _amount * PRECISION; //rewards are part of the compounding interest
+	}
+}
