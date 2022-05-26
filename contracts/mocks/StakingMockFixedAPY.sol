e39ece88d45e393a8cc17cc37bee9152675d2a53
shloyem <39220943+Shloyem@users.noreply.github.com>
2022-05-26 01:48:32 -0400

    Add mock contract to StakingRewardsFixedAPY to be able to test it

---
+++ contracts/mocks/StakingMockFixedAPY.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "../staking/utils/StakingRewardsFixedAPY.sol";
+
+contract StakingMockFixedAPY is StakingRewardsFixedAPY {
+	constructor(uint128 _interestRatePerBlock)
+		StakingRewardsFixedAPY(_interestRatePerBlock)
+	{}
+
+	function setAPY(uint128 _interestRatePerBlock) public {
+		_setAPY(_interestRatePerBlock);
+	}
+
+	function compound() public view returns (uint256 compoundedPrinciple) {
+		return _compound();
+	}
+
+	function withdraw(address _from, uint256 _amount)
+		public
+		returns (uint256 depositComponent, uint256 rewardComponent)
+	{
+		return withdraw(_from, _amount);
+	}
+
+	function stake(
+		address _from,
+		uint256 _amount,
+		uint32 _donationRatio
+	) public {
+		_stake(_from, _amount, _donationRatio);
+	}
+
+	function undoReward(address _to, uint256 _amount) public {
+		_undoReward(_to, _amount);
+	}
+}
