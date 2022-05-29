711604cc0867f29af93487de8b42cdea539e4257
shloyem <39220943+Shloyem@users.noreply.github.com>
2022-05-29 00:59:19 -0400

    present test status

--- test/staking/StakingRewardsFixedAPY.test.ts
+++ test/staking/StakingRewardsFixedAPY.test.ts
@@ -4,8 +4,10 @@ import { createDAO, advanceBlocks } from '../helpers';
 import { deploy } from '../../scripts/test/localOldDaoDeploy';
 
 const BN = ethers.BigNumber;
+
 // APY=5% | Blocks per year = 12*60*24*365 = 6307200
 // per block = nroot(1+0.05,numberOfBlocksPerYear) = 1000000007735630000
+const BLOCKS_ONE_YEAR = 6307200;
 const INTEREST_RATE_5APY_X64 = BN.from("1000000007735630000");   // x64 representation of same number
 const INTEREST_RATE_5APY_128 = BN.from("18446744216406738474"); // 128 representation of same number
 // APY = 10% | nroot(1+0.10,numberOfBlocksPerYear) = 1000000015111330000
@@ -30,10 +32,6 @@ describe("StakingRewardsFixedAPY - generic staking for fixed APY rewards contrac
     await goodDollar.mint(_staker.address, _amount);
     await goodDollar.connect(_staker).approve(fixedStaking.address, _amount);
     await fixedStaking.connect(_staker).stake(_staker.address, _amount, _givebackRatio);
-  }
-
-  async function withdraw(_staker, _amount) {
-    await fixedStaking.connect(_staker).withdraw(_staker.address, _amount);
   }
 
   function print(object) { // to be removed, just for debugging 
@@ -65,6 +63,7 @@ describe("StakingRewardsFixedAPY - generic staking for fixed APY rewards contrac
   beforeEach(async () => {
     const interestRatePerBlock = BN.from(INTEREST_RATE_5APY_X64);
     fixedStaking = await fixedStakingMockFactory.deploy(interestRatePerBlock);
+    await goodDollar.mint(fixedStaking.address, "10000000"); // to fund rewards
   });
 
   it("Should instantiate by constructor and get interest rate", async () => {
@@ -100,23 +99,36 @@ describe("StakingRewardsFixedAPY - generic staking for fixed APY rewards contrac
     print(statsBeforeStake1); // to be removed
 
     const stakeAmount1 = 100;
-    await stake(staker1, stakeAmount1, 10);
+    await stake(staker1, stakeAmount1, 0);
+    const info0 = await fixedStaking.stakersInfo(staker1.address);
+    print(info0); // to be removed
     const statsAfterStake1 = await fixedStaking.stats();
     print(statsAfterStake1); // to be removed
 
     const stakeAmount2 = 200;
-    await stake(staker2, stakeAmount2, 40);
+    await stake(staker2, stakeAmount2, 0);
+    const info00 = await fixedStaking.stakersInfo(staker1.address);
+    print(info00); // to be removed
     const statsAfterStake2 = await fixedStaking.stats();
     print(statsAfterStake2); // to be removed
 
-    advanceBlocks(10000);
+    advanceBlocks(BLOCKS_ONE_YEAR);
 
+    // change withdrawAmount2 to 51 to test reward withdrawal
     const withdrawAmount1 = 50, withdrawAmount2 = 50
-    await withdraw(staker1, withdrawAmount1);
+
+    await fixedStaking.connect(staker1).withdraw(staker1.address, withdrawAmount1);
+    const info1 = await fixedStaking.stakersInfo(staker1.address);
+    print(info1); // to be removed
     const statsAfterWithdraw1 = await fixedStaking.stats();
     print(statsAfterWithdraw1); // to be removed
 
-    await withdraw(staker1, withdrawAmount2);
+    const principleBeforeWithdraw = await fixedStaking.getPrinciple(staker1.address);
+    console.log({ p: principleBeforeWithdraw.toString() }) // to be removed
+
+    await fixedStaking.connect(staker1).withdraw(staker1.address, withdrawAmount2);
+    const info2 = await fixedStaking.stakersInfo(staker1.address);
+    print(info2); // to be removed
     const statsAfterWithdraw2 = await fixedStaking.stats();
     print(statsAfterWithdraw2); // to be removed
 
@@ -132,11 +144,11 @@ describe("StakingRewardsFixedAPY - generic staking for fixed APY rewards contrac
     expect(statsAfterWithdraw1.totalStaked.eq(statsAfterStake2.totalStaked.sub(withdrawAmount1)));
     expect(statsAfterWithdraw2.totalStaked.eq(statsAfterWithdraw1.totalStaked.sub(withdrawAmount2)));
 
-    // // totalShares
-    // expect(statsAfterStake1.totalShares.eq(statsBeforeStake1.totalShares.add(stakeAmount1)));
-    // expect(statsAfterStake2.totalShares.eq(statsAfterStake1.totalShares.add(stakeAmount2)));
-    // expect(statsAfterWithdraw1.totalShares.eq(statsAfterStake2.totalShares.sub(withdrawAmount1)));
-    // expect(statsAfterWithdraw2.totalShares.eq(statsAfterWithdraw1.totalShares.sub(withdrawAmount2)));
+    // totalShares
+    expect(statsAfterStake1.totalShares.gt(statsBeforeStake1.totalShares));
+    expect(statsAfterStake2.totalShares.gt(statsAfterStake1.totalShares));
+    expect(statsAfterWithdraw1.totalShares.lt(statsAfterStake2.totalShares));
+    expect(statsAfterWithdraw2.totalShares.lt(statsAfterWithdraw1.totalShares));
 
     // // totalRewardsPaid
     // expect(statsAfterStake1.totalRewardsPaid.eq(statsBeforeStake1.totalRewardsPaid.add(stakeAmount1)));
@@ -144,11 +156,11 @@ describe("StakingRewardsFixedAPY - generic staking for fixed APY rewards contrac
     // expect(statsAfterWithdraw1.totalRewardsPaid.eq(statsAfterStake2.totalRewardsPaid.sub(withdrawAmount1)));
     // expect(statsAfterWithdraw2.totalRewardsPaid.eq(statsAfterWithdraw1.totalRewardsPaid.sub(withdrawAmount2)));
 
-    // // principle
-    // expect(statsAfterStake1.principle.eq(statsBeforeStake1.principle.add(stakeAmount1)));
-    // expect(statsAfterStake2.principle.eq(statsAfterStake1.principle.add(stakeAmount2)));
-    // expect(statsAfterWithdraw1.principle.eq(statsAfterStake2.principle.sub(withdrawAmount1)));
-    // expect(statsAfterWithdraw2.principle.eq(statsAfterWithdraw1.principle.sub(withdrawAmount2)));
+    // principle
+    expect(statsAfterStake1.principle.gt(statsBeforeStake1.principle));
+    expect(statsAfterStake2.principle.gt(statsAfterStake1.principle));
+    expect(statsAfterWithdraw1.principle.lt(statsAfterStake2.principle));
+    expect(statsAfterWithdraw2.principle.lt(statsAfterWithdraw1.principle));
   });
 
   // in the next few tests check after running a certain scenario 
