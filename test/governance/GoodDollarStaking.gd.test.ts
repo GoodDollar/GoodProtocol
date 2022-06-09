55fa59739f9ae2215ac3212d8d91862df31e30f7
sirpy <hadarr@gmail.com>
2022-06-09 17:38:34 +0300

    refactor: staking gd rewards tests

--- test/governance/GoodDollarStaking.gd.test.ts
+++ test/governance/GoodDollarStaking.gd.test.ts
@@ -1,9 +1,7 @@
 import { default as hre, ethers, upgrades, waffle } from "hardhat";
-import { BigNumber, Contract, Signer } from "ethers";
-import { deployMockContract, MockContract } from "ethereum-waffle";
+import { Contract, Signer } from "ethers";
 import { expect } from "chai";
 import {
-  GoodMarketMaker,
   GoodReserveCDai,
   GReputation,
   GoodDollarStaking,
@@ -31,62 +29,48 @@ describe("GoodDollarStaking - check fixed APY G$ rewards", () => {
   let dai: Contract;
   let cDAI: Contract;
   let goodReserve: GoodReserveCDai;
-  let governanceStaking: GoodDollarStaking;
-  let goodFundManager: Contract;
   let grep: GReputation;
   let avatar,
     goodDollar,
-    marketMaker: GoodMarketMaker,
     controller,
     founder,
-    staker,
-    staker2,
-    staker3,
     schemeMock,
     signers,
     nameService,
     setDAOAddress,
     setSchemes,
-    runAsAvatarOnly;
+    runAsAvatarOnly,
+    staker1,
+    staker2;
 
   before(async () => {
-    [founder, staker, staker2, staker3, ...signers] = await ethers.getSigners();
+    [founder, staker1, staker2, ...signers] = await ethers.getSigners();
     schemeMock = signers.pop();
     const cdaiFactory = await ethers.getContractFactory("cDAIMock");
-    const goodFundManagerFactory = await ethers.getContractFactory(
-      "GoodFundManager"
-    );
-    const governanceStakingFactory = await ethers.getContractFactory(
-      "GoodDollarStaking"
-    );
 
     let {
       controller: ctrl,
       avatar: av,
       gd,
       identity,
-      daoCreator,
       nameService: ns,
       setDAOAddress: sda,
-      setSchemes: ss,
-      marketMaker: mm,
       daiAddress,
       cdaiAddress,
       reserve,
       reputation,
-      setReserveToken,
-      runAsAvatarOnly: raao
+      runAsAvatarOnly: ras,
+      setSchemes: ss
     } = await createDAO();
 
+    setSchemes = ss;
+    runAsAvatarOnly = ras;
     dai = await ethers.getContractAt("DAIMock", daiAddress);
     cDAI = await ethers.getContractAt("cDAIMock", cdaiAddress);
-
-    setSchemes = ss;
     avatar = av;
     controller = ctrl;
     setDAOAddress = sda;
     nameService = ns;
-    runAsAvatarOnly = raao;
     goodReserve = reserve as GoodReserveCDai;
     console.log("deployed dao", {
       founder: founder.address,
@@ -95,43 +79,22 @@ describe("GoodDollarStaking - check fixed APY G$ rewards", () => {
       controller,
       avatar
     });
-    goodFundManager = await upgrades.deployProxy(
-      goodFundManagerFactory,
-      [nameService.address],
-      { kind: "uups" }
-    );
+
     grep = (await ethers.getContractAt(
       "GReputation",
       reputation
     )) as GReputation;
-    console.log("Deployed goodfund manager", {
-      manager: goodFundManager.address
-    });
+
     goodDollar = await ethers.getContractAt("IGoodDollar", gd);
-
-    marketMaker = mm;
-
-    console.log("setting permissions...");
-
-    setDAOAddress("CDAI", cDAI.address);
-    setDAOAddress("DAI", dai.address);
 
     //This set addresses should be another function because when we put this initialization of addresses in initializer then nameservice is not ready yet so no proper addresses
     await goodReserve.setAddresses();
-
-    await setDAOAddress("MARKET_MAKER", marketMaker.address);
-    await setDAOAddress("FUND_MANAGER", goodFundManager.address);
   });
 
-  async function stake(
-    _staker,
-    _amount,
-    _givebackRatio,
-    stakingContract
-  ) {
+  async function stake(_staker, _amount, _givebackRatio, stakingContract) {
     await goodDollar.mint(_staker.address, _amount);
-    await goodDollar.approve(stakingContract.address, _amount);
-    await stakingContract.stake(_amount, _givebackRatio);
+    await goodDollar.connect(_staker).approve(stakingContract.address, _amount);
+    await stakingContract.connect(_staker).stake(_amount, _givebackRatio);
   }
 
   const fixture = async (wallets, provider) => {
@@ -173,7 +136,31 @@ describe("GoodDollarStaking - check fixed APY G$ rewards", () => {
 
     await setDAOAddress("GDAO_STAKING", staking.address);
 
-    return { staking };
+    const mintBurnWrapperFactory = await ethers.getContractFactory(
+      "GoodDollarMintBurnWrapper"
+    );
+    let goodDollarMintBurnWrapper = (await upgrades.deployProxy(
+      mintBurnWrapperFactory,
+      [1, INITIAL_CAP, avatar, nameService.address],
+      { kind: "uups" }
+    )) as unknown as GoodDollarMintBurnWrapper;
+    await setSchemes([goodDollarMintBurnWrapper.address]);
+    await setDAOAddress("MintBurnWrapper", goodDollarMintBurnWrapper.address);
+
+    let encodedCall = goodDollarMintBurnWrapper.interface.encodeFunctionData(
+      "addMinter",
+      [staking.address, INITIAL_CAP, INITIAL_CAP, 30, true]
+    );
+
+    const ictrl = await ethers.getContractAt(
+      "Controller",
+      controller,
+      schemeMock
+    );
+
+    await ictrl.genericCall(goodDollar.address, encodedCall, avatar, 0);
+
+    return { staking: staking.connect(staker1) };
   };
 
   const fixture_upgradeTest = async (wallets, provider) => {
@@ -215,14 +202,18 @@ describe("GoodDollarStaking - check fixed APY G$ rewards", () => {
     const statsBefore = await staking.stats();
     const PRECISION = await staking.PRECISION();
 
-    await stake(founder, STAKE_AMOUNT, DONATION_30_PERCENT, staking);
+    await stake(staker1, STAKE_AMOUNT, DONATION_30_PERCENT, staking);
 
-    const info = await staking.stakersInfo(founder.address);
+    const info = await staking.stakersInfo(staker1.address);
     expect(info.deposit).to.equal(STAKE_AMOUNT);
     expect(info.rewardsPaid).to.equal(0);
     expect(info.rewardsDonated).to.equal(0);
-    expect(info.shares).to.equal((await staking.SHARE_DECIMALS()).mul(STAKE_AMOUNT));
-    expect(info.avgDonationRatio).to.equal((await staking.PRECISION()).mul(DONATION_30_PERCENT));
+    expect(info.shares).to.equal(
+      (await staking.SHARE_DECIMALS()).mul(STAKE_AMOUNT)
+    );
+    expect(info.avgDonationRatio).to.equal(
+      (await staking.PRECISION()).mul(DONATION_30_PERCENT)
+    );
 
     const stats = await staking.stats();
     expect(stats.lastUpdateBlock.gt(statsBefore.lastUpdateBlock));
@@ -236,46 +227,17 @@ describe("GoodDollarStaking - check fixed APY G$ rewards", () => {
     expect(stats.principle).to.equal(PRECISION.mul(STAKE_AMOUNT));
   });
 
-  xit("should withdraw from deposit and undo rewards if unable to mint rewards", async () => {
+  it("should withdraw from deposit and undo rewards if unable to mint rewards", async () => {
     //test that withdraw is success for deposit part even if call to GoodDollarMintBurnWrapper fails
     const { staking } = await waffle.loadFixture(fixture_ready);
 
-    const mintBurnWrapperFactory = await ethers.getContractFactory("GoodDollarMintBurnWrapper");
-    let goodDollarMintBurnWrapper = (await upgrades.deployProxy(
-      mintBurnWrapperFactory,
-      [1, INITIAL_CAP, avatar, nameService.address],
-      { kind: "uups" }
-    )) as unknown as GoodDollarMintBurnWrapper;
-    await setSchemes([goodDollarMintBurnWrapper.address]);
-    await setDAOAddress("MintBurnWrapper", goodDollarMintBurnWrapper.address);
-
-    let encodedCall = goodDollarMintBurnWrapper.interface.encodeFunctionData("addMinter", [
-      staking.address,
-      INITIAL_CAP,
-      INITIAL_CAP,
-      30,
-      true
-    ]);
-
-    const ictrl = await ethers.getContractAt(
-      "Controller",
-      controller,
-      schemeMock
-    );
-
-    await ictrl.genericCall(goodDollar.address, encodedCall, avatar, 0);
-
-    await stake(founder, STAKE_AMOUNT, DONATION_30_PERCENT, staking);
+    await stake(staker1, STAKE_AMOUNT, DONATION_30_PERCENT, staking);
     await advanceBlocks(BLOCKS_ONE_YEAR);
-    await staking.withdrawStake(STAKE_AMOUNT);
+    await staking.withdrawStake(ethers.constants.MaxUint256);
+    expect(await goodDollar.balanceOf(staker1)).to.eq(STAKE_AMOUNT); //we expect only the stake to have been withdrawn successfully
   });
 
   it("should withdraw rewards after mint rewards is enabled again", async () => {});
-
-  it("should get G$ rewards when withdrawing", async () => {
-    //need to make sure GoodDollarMintBurnWrapper is deployed or some mock, so it can mint rewards
-    //test that G$ balance is equal to principle after some interest was earned
-  });
 
   it("should not perform upgrade when not deadline", async () => {
     const { staking } = await waffle.loadFixture(fixture_upgradeTest);
@@ -316,24 +278,28 @@ describe("GoodDollarStaking - check fixed APY G$ rewards", () => {
     const beforeSetInterestRateIn128 = await staking.interestRatePerBlockX64();
     expect(beforeSetInterestRateIn128).to.equal(INTEREST_RATE_5APY_128);
 
-    await runAsAvatarOnly(staking, "setGdApy(uint128)", INTEREST_RATE_10APY_X64);
+    await runAsAvatarOnly(
+      staking,
+      "setGdApy(uint128)",
+      INTEREST_RATE_10APY_X64
+    );
 
     // after set, APY is 10%
     const afterSetInterestRateIn128 = await staking.interestRatePerBlockX64();
     expect(afterSetInterestRateIn128).to.equal(INTEREST_RATE_10APY_128);
   });
 
-  xit("should withdraw only rewards when calling withdrawRewards", async () => {
+  it("should withdraw only rewards when calling withdrawRewards", async () => {
     const { staking } = await waffle.loadFixture(fixture_ready);
 
     // collect 350 earned rewards: 10,000 * 5%APY = 500 total rewards, minus 30% donation
-    await stake(founder, STAKE_AMOUNT, DONATION_30_PERCENT, staking);
+    await stake(staker1, STAKE_AMOUNT, DONATION_30_PERCENT, staking);
     await advanceBlocks(BLOCKS_ONE_YEAR);
-    const infoBefore = await staking.stakersInfo(founder.address);
+    const infoBefore = await staking.stakersInfo(staker1.address);
 
     await staking.withdrawRewards();
 
-    const info = await staking.stakersInfo(founder.address);
+    const info = await staking.stakersInfo(staker1.address);
     expect(info.deposit).to.equal(infoBefore.deposit).to.equal(STAKE_AMOUNT);
     expect(info.rewardsPaid).to.equal(350);
     expect(info.rewardsDonated).to.equal(150);
