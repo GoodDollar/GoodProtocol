71c0ef27145900f88e251cb876f266b152915079
sirpy <hadar@gooddollar.org>
2024-08-08 14:50:50 +0300

    add: bulkwhitelist helper

--- scripts/fv.ts
+++ scripts/fv.ts
@@ -1,8 +1,21 @@
-import fetch from "cross-fetch";
 import fs from "fs";
 import { chunk, uniqBy } from "lodash";
-import { bulkIsWhitelisted, bulkLastAuth } from "./utils";
+import delay from "delay";
 
+import { bulkIsWhitelisted, bulkIsWhitelistedFuse, bulkLastAuth } from "./utils";
+
+const fetchImages = async a => {
+  const i1 = await fetch("http://localhost:9090/enrollment-3d/" + a[1]).then(_ => _.json());
+  const i2 = await fetch("http://localhost:9090/enrollment-3d/" + a[2]).then(_ => _.json());
+  if (i1.auditTrailBase64 && i2.auditTrailBase64) {
+    fs.writeFileSync("fvimages/" + a.join("_") + "-a.jpg", i1.auditTrailBase64, {
+      encoding: "base64"
+    });
+    fs.writeFileSync("fvimages/" + a.join("_") + "-b.jpg", i2.auditTrailBase64, {
+      encoding: "base64"
+    });
+  } else console.log("not found", a, !!i1.auditTrailBase64, !!i2.auditTrailBase64);
+};
 //create tunnel to fv server ssh -L 9090:server:8080 -N user@server -i sshkey
 const main = async () => {
   const data = JSON.parse(fs.readFileSync("fvtriplets2.txt").toString());
@@ -20,20 +33,123 @@ const main = async () => {
     return fs.existsSync(key) === false;
   });
   console.log({ notfetched });
-  const ps = notfetched.map(async a => {
-    const i1 = await fetch("http://localhost:9090/enrollment-3d/" + a[1]).then(_ => _.json());
-    const i2 = await fetch("http://localhost:9090/enrollment-3d/" + a[2]).then(_ => _.json());
-    if (i1.auditTrailBase64 && i2.auditTrailBase64) {
-      fs.writeFileSync("fvimages/" + a.join("_") + "-a.jpg", i1.auditTrailBase64, {
-        encoding: "base64"
-      });
-      fs.writeFileSync("fvimages/" + a.join("_") + "-b.jpg", i2.auditTrailBase64, {
-        encoding: "base64"
-      });
-    } else console.log("not found", a, !!i1.auditTrailBase64, !!i2.auditTrailBase64);
-  });
+  const ps = notfetched.map(fetchImages);
 
   await Promise.all(ps);
+};
+
+// check which ids have been indexed, if not then make sure they are whitelisted
+const checkIndexedOrDelete = async () => {
+  const data = JSON.parse(
+    "[" + fs.readFileSync("fvids.json").toString().replace(/\n$/, "").replaceAll(/\n/g, ",\n") + "]"
+  );
+  const triplets = uniqBy(data, _ => _["wallet"]);
+
+  const accounts = triplets.map(_ => _["wallet"]);
+  console.log("Total accounts:", accounts.length, accounts[0]);
+  let whitelisted = [];
+  for (let batch of chunk(accounts, 200)) {
+    whitelisted.push(...(await bulkIsWhitelistedFuse(batch)));
+  }
+  const failed = triplets.filter(_ => whitelisted.includes(_["wallet"]) === false);
+
+  console.log("Total whitelisted:", whitelisted.length);
+  console.log("Total not whitelisted:", failed.length);
+  let ps = [];
+
+  ps = [];
+  for (let record of triplets) {
+    const isEnrolled = fetch("http://localhost:9090/enrollment-3d/" + record["identifier"].slice(0, 42))
+      .then(_ => _.json())
+      .then(_ => _.success);
+    // const isEnrolled = "unknown";
+    const isIndexed = fetch("http://localhost:9090/3d-db/get", {
+      method: "POST",
+      body: JSON.stringify({ identifier: record["identifier"].slice(0, 42), groupName: "GoodDollar" })
+    })
+      .then(_ => _.json())
+      .then(_ => _.success);
+
+    ps.push(
+      Promise.all([isEnrolled, isIndexed]).then(_ => ({
+        ...record,
+        isEnrolled: _[0],
+        isIndexed: _[1],
+        isWhitelisted: whitelisted.includes(record.wallet)
+      }))
+    );
+    if (ps.length % 50 === 0) {
+      console.log("waiting...", ps.length);
+      await Promise.all(ps);
+    }
+  }
+  console.log("waiting...", ps.length);
+  const r = await Promise.all(ps);
+  const toDelete = r.filter(
+    _ => (_.isWhitelisted && (!_.isEnrolled || !_.isIndexed)) || (!_.isWhitelisted && _.isIndexed && !_.isEnrolled)
+  );
+  const toWhitelist = r.filter(_ => !_.isWhitelisted && _.isEnrolled && _.isIndexed);
+
+  console.log("toDelete:", toDelete.length);
+
+  console.log("toWhitelist:", toWhitelist.length);
+  fs.writeFileSync("fvIndexedOrEnrolled.json", JSON.stringify(r));
+};
+
+const fixInvalidIndexed = async () => {
+  const indexedOrEnrolled = JSON.parse(fs.readFileSync("fvIndexedOrEnrolled.json").toString());
+  const toDelete = indexedOrEnrolled.filter(
+    _ => (_.isWhitelisted && (!_.isEnrolled || !_.isIndexed)) || (!_.isWhitelisted && _.isIndexed && !_.isEnrolled)
+  );
+  const toWhitelist = indexedOrEnrolled.filter(_ => !_.isWhitelisted && _.isEnrolled && _.isIndexed);
+
+  console.log("toDelete:", toDelete.length);
+
+  console.log("toWhitelist:", toWhitelist.length);
+
+  console.log(toWhitelist);
+  return;
+  let ps = [];
+  for (let record of toWhitelist) {
+    ps.push(fetch(`https://goodserver.gooddollar.org/syncWhitelist/${record.wallet}`).then(_ => _.json()));
+    if (ps.length > 0 && ps.length % 3 === 0) {
+      console.log("waiting...", ps.length);
+      await Promise.all(ps).catch(e => console.log(e));
+      console.log("waiting 60 seconds for rate limit");
+      await delay(50000);
+    }
+  }
+  if (ps.length > 0) {
+    console.log("waiting...", ps.length);
+    await Promise.all(ps).catch(e => console.log(e));
+  }
+
+  const removeWhitelist = toDelete.filter(_ => _.isWhitelisted).map(_ => _.wallet);
+  // removeWhitelist.push(...toWhitelist.map(_ => _.wallet));
+  ps = [];
+  for (let record of toDelete) {
+    if (record.isIndexed) {
+      // console.log("removing record from index and queuing for whitelist removal", record.wallet);
+      const isIndexed = fetch("http://localhost:9090/3d-db/delete", {
+        method: "POST",
+        body: JSON.stringify({ identifier: record["identifier"].slice(0, 42), groupName: "GoodDollar" })
+      }).then(_ => _.json());
+
+      ps.push(
+        isIndexed.then(_ => {
+          if (!_.success) throw new Error(`delete index failed: ${record.wallet}`);
+        })
+      );
+    }
+    if (ps.length > 0 && ps.length % 10 === 0) {
+      console.log("waiting...", ps.length);
+      await Promise.all(ps).catch(e => console.log(e));
+    }
+  }
+  console.log("waiting...", ps.length);
+  await Promise.all(ps).catch(e => console.log(e));
+  console.log({ removeWhitelist }, removeWhitelist.length);
+  fs.writeFileSync("removeWhitelist.json", JSON.stringify(removeWhitelist));
 };
 
 const deleteIdentifiers = async password => {
@@ -89,7 +205,8 @@ const deleteIdentifiers = async password => {
   const res = await Promise.all(ps);
   console.log(res);
 };
-
+// checkIndexedOrDelete();
+fixInvalidIndexed();
 // main();
-console.log(process.env.ADMIN_PASSWORD);
-deleteIdentifiers(process.env.ADMIN_PASSWORD);
+// console.log(process.env.ADMIN_PASSWORD);
+// deleteIdentifiers(process.env.ADMIN_PASSWORD);
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -13,6 +13,17 @@ export const bulkIsWhitelisted = async (accounts: Array<String>) => {
   const whitelisted = accounts.filter((v, i) => result[i]);
   return whitelisted;
 };
+export const bulkIsWhitelistedFuse = async (accounts: Array<String>) => {
+  setMulticallAddress(122, "0x3CE6158b7278Bf6792e014FA7B4f3c6c46fe9410");
+  setMulticallAddress(42220, "0x75F59534dd892c1f8a7B172D639FA854D529ada3");
+  const provider = new ethers.providers.JsonRpcProvider("https://rpc.fuse.io");
+  const ethcallProvider = new Provider(provider, 122);
+  const identityContract = new Contract("0x2F9C28de9e6d44b71B91b8BA337A5D82e308E7BE", Identity.abi);
+  const calls = accounts.map(d => identityContract.isWhitelisted(d));
+  const result = await ethcallProvider.all(calls);
+  const whitelisted = accounts.filter((v, i) => result[i]);
+  return whitelisted;
+};
 
 export const bulkLastAuth = async (accounts: Array<String>) => {
   setMulticallAddress(122, "0x3CE6158b7278Bf6792e014FA7B4f3c6c46fe9410");
