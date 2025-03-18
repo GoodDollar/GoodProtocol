5dc14e7850fbd0c5c6a3e3eb23f09c19d6dfdd3b
sirpy <hadar@gooddollar.org>
2025-03-18 16:22:37 +0200

    add: full broker interface

--- contracts/MentoInterfaces.sol
+++ contracts/MentoInterfaces.sol
@@ -531,134 +531,216 @@ interface IGoodDollarExchangeProvider {
  * @title Broker Interface for trader functions
  * @notice The broker is responsible for executing swaps and keeping track of trading limits.
  */
+
+interface ITradingLimits {
+  /**
+   * @dev The State struct contains the current state of a trading limit config.
+   * @param lastUpdated0 The timestamp of the last reset of netflow0.
+   * @param lastUpdated1 The timestamp of the last reset of netflow1.
+   * @param netflow0 The current netflow of the asset for limit0.
+   * @param netflow1 The current netflow of the asset for limit1.
+   * @param netflowGlobal The current netflow of the asset for limitGlobal.
+   */
+  struct State {
+    uint32 lastUpdated0;
+    uint32 lastUpdated1;
+    int48 netflow0;
+    int48 netflow1;
+    int48 netflowGlobal;
+  }
+
+  /**
+   * @dev The Config struct contains the configuration of trading limits.
+   * @param timestep0 The time window in seconds for limit0.
+   * @param timestep1 The time window in seconds for limit1.
+   * @param limit0 The limit0 for the asset.
+   * @param limit1 The limit1 for the asset.
+   * @param limitGlobal The global limit for the asset.
+   * @param flags A bitfield of flags to enable/disable the individual limits.
+   */
+  struct Config {
+    uint32 timestep0;
+    uint32 timestep1;
+    int48 limit0;
+    int48 limit1;
+    int48 limitGlobal;
+    uint8 flags;
+  }
+}
+
+/*
+ * @title Broker Interface for trader functions
+ * @notice The broker is responsible for executing swaps and keeping track of trading limits.
+ */
 interface IBroker {
-	/**
-	 * @notice Emitted when a swap occurs.
-	 * @param exchangeProvider The exchange provider used.
-	 * @param exchangeId The id of the exchange used.
-	 * @param trader The user that initiated the swap.
-	 * @param tokenIn The address of the token that was sold.
-	 * @param tokenOut The address of the token that was bought.
-	 * @param amountIn The amount of token sold.
-	 * @param amountOut The amount of token bought.
-	 */
-	event Swap(
-		address exchangeProvider,
-		bytes32 indexed exchangeId,
-		address indexed trader,
-		address indexed tokenIn,
-		address tokenOut,
-		uint256 amountIn,
-		uint256 amountOut
-	);
+  /**
+   * @notice Emitted when a swap occurs.
+   * @param exchangeProvider The exchange provider used.
+   * @param exchangeId The id of the exchange used.
+   * @param trader The user that initiated the swap.
+   * @param tokenIn The address of the token that was sold.
+   * @param tokenOut The address of the token that was bought.
+   * @param amountIn The amount of token sold.
+   * @param amountOut The amount of token bought.
+   */
+  event Swap(
+    address exchangeProvider,
+    bytes32 indexed exchangeId,
+    address indexed trader,
+    address indexed tokenIn,
+    address tokenOut,
+    uint256 amountIn,
+    uint256 amountOut
+  );
 
-	/**
-	 * @notice Initialize the broker with the exchange providers and reserves.
-	 * @param _exchangeProviders The addresses of the exchange providers.
-	 * @param _reserves The addresses of the reserves.
-	 */
-	function initialize(
-		address[] calldata _exchangeProviders,
-		address[] calldata _reserves
-	) external;
+  /**
+   * @notice Emitted when a new trading limit is configured.
+   * @param exchangeId the exchangeId to target.
+   * @param token the token to target.
+   * @param config the new trading limits config.
+   */
+  event TradingLimitConfigured(bytes32 exchangeId, address token, ITradingLimits.Config config);
 
-	/**
-	 * @notice returns whether an exchange provider is registered.
-	 * @param exchangeProvider the address of the exchange provider.
-	 * @return isExchangeProvider true if the exchange provider is registered.
-	 */
-	function isExchangeProvider(
-		address exchangeProvider
-	) external view returns (bool);
+  /**
+   * @notice Allows the contract to be upgradable via the proxy.
+   * @param _exchangeProviders The addresses of the ExchangeProvider contracts.
+   * @param _reserves The address of the Reserve contract.
+   */
+  function initialize(address[] calldata _exchangeProviders, address[] calldata _reserves) external;
 
-	/**
-	 * @notice returns the reserve address for an exchange provider.
-	 * @param exchangeProvider the address of the exchange provider.
-	 * @return reserve the address of the reserve.
-	 */
-	function exchangeReserve(
-		address exchangeProvider
-	) external view returns (address);
+  /**
+   * @notice Set the reserves for the exchange providers.
+   * @param _exchangeProviders The addresses of the ExchangeProvider contracts.
+   * @param _reserves The addresses of the Reserve contracts.
+   */
+  function setReserves(address[] calldata _exchangeProviders, address[] calldata _reserves) external;
 
-	/**
-	 * @notice Execute a token swap with fixed amountIn.
-	 * @param exchangeProvider the address of the exchange provider for the pair.
-	 * @param exchangeId The id of the exchange to use.
-	 * @param tokenIn The token to be sold.
-	 * @param tokenOut The token to be bought.
-	 * @param amountIn The amount of tokenIn to be sold.
-	 * @param amountOutMin Minimum amountOut to be received - controls slippage.
-	 * @return amountOut The amount of tokenOut to be bought.
-	 */
-	function swapIn(
-		address exchangeProvider,
-		bytes32 exchangeId,
-		address tokenIn,
-		address tokenOut,
-		uint256 amountIn,
-		uint256 amountOutMin
-	) external returns (uint256 amountOut);
+  /**
+   * @notice Add an exchange provider to the list of providers.
+   * @param exchangeProvider The address of the exchange provider to add.
+   * @param reserve The address of the reserve used by the exchange provider.
+   * @return index The index of the newly added specified exchange provider.
+   */
+  function addExchangeProvider(address exchangeProvider, address reserve) external returns (uint256 index);
 
-	/**
-	 * @notice Execute a token swap with fixed amountOut.
-	 * @param exchangeProvider the address of the exchange provider for the pair.
-	 * @param exchangeId The id of the exchange to use.
-	 * @param tokenIn The token to be sold.
-	 * @param tokenOut The token to be bought.
-	 * @param amountOut The amount of tokenOut to be bought.
-	 * @param amountInMax Maximum amount of tokenIn that can be traded.
-	 * @return amountIn The amount of tokenIn to be sold.
-	 */
-	function swapOut(
-		address exchangeProvider,
-		bytes32 exchangeId,
-		address tokenIn,
-		address tokenOut,
-		uint256 amountOut,
-		uint256 amountInMax
-	) external returns (uint256 amountIn);
+  /**
+   * @notice Remove an exchange provider from the list of providers.
+   * @param exchangeProvider The address of the exchange provider to remove.
+   * @param index The index of the exchange provider being removed.
+   */
+  function removeExchangeProvider(address exchangeProvider, uint256 index) external;
 
-	/**
-	 * @notice Calculate amountOut of tokenOut received for a given amountIn of tokenIn.
-	 * @param exchangeProvider the address of the exchange provider for the pair.
-	 * @param exchangeId The id of the exchange to use.
-	 * @param tokenIn The token to be sold.
-	 * @param tokenOut The token to be bought.
-	 * @param amountIn The amount of tokenIn to be sold.
-	 * @return amountOut The amount of tokenOut to be bought.
-	 */
-	function getAmountOut(
-		address exchangeProvider,
-		bytes32 exchangeId,
-		address tokenIn,
-		address tokenOut,
-		uint256 amountIn
-	) external view returns (uint256 amountOut);
+  /**
+   * @notice Calculate amountIn of tokenIn needed for a given amountOut of tokenOut.
+   * @param exchangeProvider the address of the exchange provider for the pair.
+   * @param exchangeId The id of the exchange to use.
+   * @param tokenIn The token to be sold.
+   * @param tokenOut The token to be bought.
+   * @param amountOut The amount of tokenOut to be bought.
+   * @return amountIn The amount of tokenIn to be sold.
+   */
+  function getAmountIn(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountOut
+  ) external view returns (uint256 amountIn);
 
-	/**
-	 * @notice Calculate amountIn of tokenIn needed for a given amountOut of tokenOut.
-	 * @param exchangeProvider the address of the exchange provider for the pair.
-	 * @param exchangeId The id of the exchange to use.
-	 * @param tokenIn The token to be sold.
-	 * @param tokenOut The token to be bought.
-	 * @param amountOut The amount of tokenOut to be bought.
-	 * @return amountIn The amount of tokenIn to be sold.
-	 */
-	function getAmountIn(
-		address exchangeProvider,
-		bytes32 exchangeId,
-		address tokenIn,
-		address tokenOut,
-		uint256 amountOut
-	) external view returns (uint256 amountIn);
+  /**
+   * @notice Calculate amountOut of tokenOut received for a given amountIn of tokenIn.
+   * @param exchangeProvider the address of the exchange provider for the pair.
+   * @param exchangeId The id of the exchange to use.
+   * @param tokenIn The token to be sold.
+   * @param tokenOut The token to be bought.
+   * @param amountIn The amount of tokenIn to be sold.
+   * @return amountOut The amount of tokenOut to be bought.
+   */
+  function getAmountOut(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountIn
+  ) external view returns (uint256 amountOut);
 
-	/**
-	 * @notice Get the list of registered exchange providers.
-	 * @dev This can be used by UI or clients to discover all pairs.
-	 * @return exchangeProviders the addresses of all exchange providers.
-	 */
-	function getExchangeProviders()
-		external
-		view
-		returns (address[] memory exchangeProviders);
+  /**
+   * @notice Execute a token swap with fixed amountIn.
+   * @param exchangeProvider the address of the exchange provider for the pair.
+   * @param exchangeId The id of the exchange to use.
+   * @param tokenIn The token to be sold.
+   * @param tokenOut The token to be bought.
+   * @param amountIn The amount of tokenIn to be sold.
+   * @param amountOutMin Minimum amountOut to be received - controls slippage.
+   * @return amountOut The amount of tokenOut to be bought.
+   */
+  function swapIn(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountIn,
+    uint256 amountOutMin
+  ) external returns (uint256 amountOut);
+
+  /**
+   * @notice Execute a token swap with fixed amountOut.
+   * @param exchangeProvider the address of the exchange provider for the pair.
+   * @param exchangeId The id of the exchange to use.
+   * @param tokenIn The token to be sold.
+   * @param tokenOut The token to be bought.
+   * @param amountOut The amount of tokenOut to be bought.
+   * @param amountInMax Maximum amount of tokenIn that can be traded.
+   * @return amountIn The amount of tokenIn to be sold.
+   */
+  function swapOut(
+    address exchangeProvider,
+    bytes32 exchangeId,
+    address tokenIn,
+    address tokenOut,
+    uint256 amountOut,
+    uint256 amountInMax
+  ) external returns (uint256 amountIn);
+
+  /**
+   * @notice Permissionless way to burn stables from msg.sender directly.
+   * @param token The token getting burned.
+   * @param amount The amount of the token getting burned.
+   * @return True if transaction succeeds.
+   */
+  function burnStableTokens(address token, uint256 amount) external returns (bool);
+
+  /**
+   * @notice Configure trading limits for an (exchangeId, token) tuple.
+   * @dev Will revert if the configuration is not valid according to the TradingLimits library.
+   * Resets existing state according to the TradingLimits library logic.
+   * Can only be called by owner.
+   * @param exchangeId the exchangeId to target.
+   * @param token the token to target.
+   * @param config the new trading limits config.
+   */
+  function configureTradingLimit(bytes32 exchangeId, address token, ITradingLimits.Config calldata config) external;
+
+  /**
+   * @notice Get the list of registered exchange providers.
+   * @dev This can be used by UI or clients to discover all pairs.
+   * @return exchangeProviders the addresses of all exchange providers.
+   */
+  function getExchangeProviders() external view returns (address[] memory);
+
+  /**
+   * @notice Get the address of the exchange provider at a given index.
+   * @dev Auto-generated getter for the exchangeProviders array.
+   * @param index The index of the exchange provider.
+   * @return exchangeProvider The address of the exchange provider.
+   */
+  function exchangeProviders(uint256 index) external view returns (address exchangeProvider);
+
+  /**
+   * @notice Check if a given address is an exchange provider.
+   * @dev Auto-generated getter for the isExchangeProvider mapping.
+   * @param exchangeProvider The address to check.
+   * @return isExchangeProvider True if the address is an exchange provider, false otherwise.
+   */
+  function isExchangeProvider(address exchangeProvider) external view returns (bool);
 }
