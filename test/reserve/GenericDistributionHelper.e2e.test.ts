abe16ff8acce657b7a454b3f3eef318146dcd40a
blueogin <grilucosy@gmail.com>
2026-01-21 10:32:10 -0500

    refactor: streamline before hook in GenericDistributionHelper E2E tests

--- test/reserve/GenericDistributionHelper.e2e.test.ts
+++ test/reserve/GenericDistributionHelper.e2e.test.ts
@@ -50,11 +50,7 @@ describe("GenericDistributionHelper - XDC XSWAP E2E Test", function () {
   // this.afterAll(async function () {
   //   await networkHelpers.reset(XDC_RPC_URL);
   // });
-  before(async function () {
-    
-    // const provider = network.provider;
-    // await provider.send("hardhat_reset", [{url: XDC_RPC_URL}]);
-    await networkHelpers.reset(XDC_RPC_URL);
+  before(async function () {    
     const chainId = await ethers.provider.getNetwork().then(network => network.chainId);
     if(chainId !== XDC_CHAIN_ID) {
       this.skip();
